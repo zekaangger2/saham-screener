5df3592eb7b2c1da9dbeb016951c754dbf4318c7
zekaangger2 <zekaangger2@gmail.com>
2026-05-01 03:47:28 +0800

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -1,50 +1,211 @@
-import pandas as pd
+STOCKS = [
 
-# =====================================================
-# AMBIL SELURUH SAHAM IDX
-# =====================================================
+# =========================
+# BANK
+# =========================
 
-url = "https://www.idx.co.id/primary/TradingSummary/GetStockCode"
+"BBCA.JK","BBRI.JK","BMRI.JK","BBNI.JK","BRIS.JK",
+"ARTO.JK","BTPS.JK","BJBR.JK","BJTM.JK","BNGA.JK",
+"BNII.JK","BDMN.JK","MAYA.JK","MEGA.JK","NISP.JK",
+"PNBN.JK","BBKP.JK","BBTN.JK","AGRO.JK","BACA.JK",
 
-try:
+# =========================
+# MINING
+# =========================
 
-    df = pd.read_json(url)
+"ANTM.JK","ADRO.JK","PTBA.JK","TINS.JK","MDKA.JK",
+"ITMG.JK","MEDC.JK","ADMR.JK","BUMI.JK","DOID.JK",
+"ELSA.JK","HRUM.JK","INDY.JK","TOBA.JK","UNTR.JK",
+"NCKL.JK","DEWA.JK","BSSR.JK","KKGI.JK","MBAP.JK",
 
-    STOCKS = [
-        f"{kode}.JK"
-        for kode in df["Code"].tolist()
-    ]
+# =========================
+# TELECOM
+# =========================
 
-except:
+"TLKM.JK","ISAT.JK","EXCL.JK","FREN.JK","MTEL.JK",
 
-    # fallback jika server IDX gagal
-    STOCKS = [
+# =========================
+# CONSUMER
+# =========================
 
-        "BBCA.JK",
-        "BBRI.JK",
-        "BMRI.JK",
-        "BBNI.JK",
-        "BRIS.JK",
+"UNVR.JK","ICBP.JK","INDF.JK","MYOR.JK","ULTJ.JK",
+"SIDO.JK","STTP.JK","GOOD.JK","HOKI.JK","ROTI.JK",
+"CLEO.JK","MLBI.JK","SKBM.JK","AISA.JK","DLTA.JK",
 
-        "ANTM.JK",
-        "ADRO.JK",
-        "PTBA.JK",
-        "TINS.JK",
+# =========================
+# POULTRY
+# =========================
 
-        "TLKM.JK",
-        "ISAT.JK",
-        "EXCL.JK",
+"CPIN.JK","JPFA.JK","MAIN.JK","SIPD.JK",
 
-        "ASII.JK",
-        "UNVR.JK",
-        "ICBP.JK",
-        "INDF.JK",
+# =========================
+# PROPERTY
+# =========================
 
-        "CPIN.JK",
-        "JPFA.JK",
+"PWON.JK","BSDE.JK","CTRA.JK","SMRA.JK","DMAS.JK",
+"ASRI.JK","DILD.JK","LPKR.JK","BEST.JK","KIJA.JK",
+"BKSL.JK","MDLN.JK","JRPT.JK","MTLA.JK","PPRO.JK",
 
-        "PWON.JK",
-        "BSDE.JK",
-        "CTRA.JK"
+# =========================
+# INFRA
+# =========================
 
-    ]
+"JSMR.JK","PGAS.JK","WIKA.JK","PTPP.JK","WSKT.JK",
+"ADHI.JK","WEGE.JK","TOTL.JK","DGIK.JK","IDPR.JK",
+
+# =========================
+# HEALTHCARE
+# =========================
+
+"HEAL.JK","KLBF.JK","MIKA.JK","SILO.JK","SRAJ.JK",
+"PRDA.JK","CARE.JK","IRRA.JK","SOHO.JK","PEHA.JK",
+
+# =========================
+# TECHNOLOGY
+# =========================
+
+"GOTO.JK","BUKA.JK","DNET.JK","MCAS.JK","EMTK.JK",
+"ERAA.JK","MLPT.JK","TECH.JK","EDGE.JK","ZYRX.JK",
+
+# =========================
+# CEMENT
+# =========================
+
+"SMGR.JK","INTP.JK","SMBR.JK","WSBP.JK",
+
+# =========================
+# RETAIL
+# =========================
+
+"ACES.JK","AMRT.JK","MAPI.JK","RALS.JK","LPPF.JK",
+"MPPA.JK","CSAP.JK","SONA.JK","HERO.JK","MIDI.JK",
+
+# =========================
+# AUTOMOTIVE
+# =========================
+
+"ASII.JK","AUTO.JK","SMSM.JK","IMAS.JK","LPIN.JK",
+
+# =========================
+# TRANSPORT
+# =========================
+
+"BIRD.JK","SMDR.JK","WINS.JK","TMAS.JK","SAFE.JK",
+
+# =========================
+# ENERGY
+# =========================
+
+"AKRA.JK","MEDC.JK","PGAS.JK","ELSA.JK",
+
+# =========================
+# TOBACCO
+# =========================
+
+"HMSP.JK","GGRM.JK","WIIM.JK","RMBA.JK",
+
+# =========================
+# INDUSTRIAL
+# =========================
+
+"UNTR.JK","SRTG.JK","IPCM.JK","KAEF.JK","INKP.JK",
+"TKIM.JK","FASW.JK","ALDO.JK","ARGO.JK","KRAS.JK",
+
+# =========================
+# SHIPPING
+# =========================
+
+"SAMUDERA.JK","SMDR.JK","TMAS.JK","HATM.JK",
+
+# =========================
+# AGRI
+# =========================
+
+"AALI.JK","LSIP.JK","SIMP.JK","TBLA.JK","SGRO.JK",
+"SSMS.JK","DSNG.JK","BWPT.JK","GZCO.JK","PALM.JK",
+
+# =========================
+# INSURANCE
+# =========================
+
+"LIFE.JK","ASDM.JK","PNIN.JK","ABDA.JK",
+
+# =========================
+# AVIATION
+# =========================
+
+"GIAA.JK","CMPP.JK","IATA.JK",
+
+# =========================
+# MEDIA
+# =========================
+
+"SCMA.JK","MNCN.JK","BMTR.JK","VIVA.JK","NETV.JK",
+
+# =========================
+# TEXTILE
+# =========================
+
+"SRIL.JK","PBRX.JK","RICY.JK","POLY.JK",
+
+# =========================
+# CHEMICAL
+# =========================
+
+"TPIA.JK","BRPT.JK","ESSA.JK","BIPI.JK",
+
+# =========================
+# ADDITIONAL SMALL CAPS
+# =========================
+
+"ABMM.JK","ACES.JK","ACST.JK","ADES.JK","AGII.JK",
+"AGRS.JK","AHAP.JK","AIMS.JK","AKKU.JK","ALKA.JK",
+"AMAG.JK","AMAN.JK","AMFG.JK","ANDI.JK","APEX.JK",
+"APII.JK","ARGO.JK","ARKA.JK","ARNA.JK","ARTA.JK",
+"ASBI.JK","ASGR.JK","ASMI.JK","ASRM.JK","ASJT.JK",
+"ATIC.JK","BALI.JK","BAPA.JK","BATA.JK","BEEF.JK",
+"BELL.JK","BESS.JK","BFIN.JK","BHIT.JK","BIKA.JK",
+"BIMA.JK","BINA.JK","BIPP.JK","BKDP.JK","BLTA.JK",
+"BLUE.JK","BMAS.JK","BMHS.JK","BOGA.JK","BRAM.JK",
+"BRNA.JK","BSIM.JK","BUDI.JK","BUVA.JK","BVIC.JK",
+"CAMP.JK","CANI.JK","CARS.JK","CASA.JK","CBMF.JK",
+"CCSI.JK","CEKA.JK","CENT.JK","CFIN.JK","CINT.JK",
+"CMNP.JK","CNKO.JK","CNMA.JK","CNTX.JK","CSIS.JK",
+"CTBN.JK","DADA.JK","DAYA.JK","DEFI.JK","DEST.JK",
+"DIVA.JK","DPNS.JK","DUCK.JK","DUTI.JK","ECII.JK",
+"EPMT.JK","ESTA.JK","ESTI.JK","FAPA.JK","FIRE.JK",
+"FMII.JK","FORU.JK","GEMA.JK","GHON.JK","GJTL.JK",
+"GLVA.JK","GLOB.JK","GMFI.JK","GOLD.JK","GOLL.JK",
+"GREN.JK","HDFA.JK","HEXA.JK","HITS.JK","HOPE.JK",
+"HOTL.JK","HRTA.JK","IBST.JK","IKAI.JK","IKBI.JK",
+"IMJS.JK","IMPC.JK","INAI.JK","INCF.JK","INDR.JK",
+"INDR.JK","INDS.JK","INDX.JK","INRU.JK","INTA.JK",
+"IPOL.JK","ISSP.JK","JAWA.JK","JGLE.JK","JKON.JK",
+"JMAS.JK","KBLM.JK","KBLV.JK","KDSI.JK","KEEN.JK",
+"KIAS.JK","KICI.JK","KINO.JK","KOBX.JK","KOIN.JK",
+"KONI.JK","KPAL.JK","KREN.JK","LAND.JK","LAPD.JK",
+"LCGP.JK","LEAD.JK","LION.JK","LMAS.JK","LMPI.JK",
+"LPCK.JK","LRNA.JK","LUCK.JK","MABA.JK","MAGP.JK",
+"MARK.JK","MASA.JK","MAYA.JK","MBSS.JK","MCOR.JK",
+"MDIA.JK","MFMI.JK","MGNA.JK","MICE.JK","MIRA.JK",
+"MITI.JK","MKNT.JK","MLIA.JK","MPMX.JK","MREI.JK",
+"MSIN.JK","MTFN.JK","MYRX.JK","NELY.JK","NIKL.JK",
+"NIPS.JK","NUSA.JK","OKAS.JK","OMRE.JK","PADI.JK",
+"PANI.JK","PANS.JK","PBID.JK","PDES.JK","PEGE.JK",
+"PGLI.JK","PICO.JK","PKPK.JK","PLAN.JK","PLAS.JK",
+"POLA.JK","POOL.JK","PORT.JK","PPGL.JK","PSAB.JK",
+"PSDN.JK","PTIS.JK","PTSN.JK","PUDP.JK","PYFA.JK",
+"RAJA.JK","RBMS.JK","REAL.JK","RIGS.JK","RIMO.JK",
+"RMKE.JK","SAME.JK","SATU.JK","SBAT.JK","SCCO.JK",
+"SDMU.JK","SEMA.JK","SHIP.JK","SIMA.JK","SKLT.JK",
+"SLIS.JK","SMDM.JK","SMKL.JK","SOFA.JK","SPMA.JK",
+"SQMI.JK","SSTM.JK","STAR.JK","SUPR.JK","TALF.JK",
+"TAPG.JK","TCID.JK","TDPM.JK","TEBE.JK","TFAS.JK",
+"TFCO.JK","TGKA.JK","TIFA.JK","TINS.JK","TKGA.JK",
+"TLDN.JK","TOPS.JK","TOYS.JK","TRAM.JK","TRIN.JK",
+"TRIS.JK","TRUE.JK","TSPC.JK","TUGU.JK","UANG.JK",
+"UCID.JK","UFOE.JK","ULTJ.JK","UNIT.JK","UNSP.JK",
+"VAST.JK","VKTR.JK","WAPO.JK","WEHA.JK","WIFI.JK",
+"WOMF.JK","WOOD.JK","YELO.JK","ZONE.JK"
+
+]
